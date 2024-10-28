f9c58846d8d3756cfbcd9bb40e0458c2b6284bd2
Morgan Jones <me@morganlabs.dev>
2024-10-28 18:48:05 +0000

    feat(nixos): Option for Encrypted DNS for NetworkManager with ControlD

--- nixosModules/connectivity/networkmanager.nix
+++ nixosModules/connectivity/networkmanager.nix
@@ -12,6 +12,7 @@ with lib;
 {
   options.nixosModules.connectivity.networkmanager = {
     enable = mkEnableOption "Enable connectivity.networkmanager";
+    dns.controld.enable = mkBoolOption "Use the ControlD DNS" true;
 
     wifi = {
       enable = mkBoolOption "Enable WiFi support" false;
@@ -19,12 +20,33 @@ with lib;
     };
   };
 
-  config = mkIf cfg.enable {
-    networking = {
-      networkmanager = {
+  config =
+    mkIf cfg.enable {
+      networking.networkmanager = {
         enable = mkForce true;
         wifi.backend = mkForce (mkIfElse cfg.wifi.useIwd "iwd" "wpa_supplicant");
       };
+    }
+    // mkIf cfg.dns.controld.enable {
+      networking = {
+        networkmanager.dns = "none";
+        nameservers = [
+          "76.76.2.2#ControlD"
+          "76.76.10.2#ControlD"
+          "2606:1a40::2#ControlD"
+          "2606:1a40:1::2#ControlD"
+        ];
+      };
+
+      services.resolved = {
+        enable = true;
+        dnssec = "true";
+        domains = [ "~." ];
+        extraConfig = ''
+          DNSOverTLS=opportunistic
+          MulticastDNS=resolve
+        '';
+        llmnr = "true";
+      };
     };
-  };
 }
